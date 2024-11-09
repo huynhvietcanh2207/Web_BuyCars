87d1ef446a947915f6fdcee2d31cf6525487e81b
huynhvietcanh2207 <huynhvietcanh2004@gmail.com>
2024-11-09 10:17:10 +0700

    ád

--- DoAn_BuyCars/resources/views/favorite.blade.php
+++ DoAn_BuyCars/resources/views/favorite.blade.php
@@ -39,19 +39,19 @@
                         <div class="product-title">{{ $item->product->name }}</div>
                         <div class="product-price">{{ number_format($item->product->price, 0, ',', '.') }} VND</div>
                         <div class="icon-btn">
-                            <div class="icon-products">
-                                <i class="{{ $item->product->ProductId ? 'fas fa-heart' : 'far fa-heart' }} favorite-btn"
-                                    data-product-id="{{ $item->product->ProductId }}"></i>
-                            </div>
-                            <form action="{{ route('cart.add', $product->ProductId) }}" method="POST">
-                                @csrf
-                                <input type="hidden" name="product_id" value="{{ $product->ProductId }}">
-                                @if (auth()->check())
-                                <input type="hidden" name="user_id" value="{{ auth()->id() }}">
-                                @endif
-                                <button class="btn-add-to-cart" type="submit">Thêm vào giỏ hàng</button>
-                            </form>
-                        </div>
+                <div class="icon-products">
+                    <i class="{{ $product->is_favorited ? 'fas fa-heart' : 'far fa-heart' }} favorite-btn"
+                        data-product-id="{{ $product->ProductId }}"></i>
+                </div>
+                <form action="{{ route('cart.add', $product->ProductId) }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="product_id" value="{{ $product->ProductId }}">
+                    @if (auth()->check())
+                        <input type="hidden" name="user_id" value="{{ auth()->id() }}">
+                    @endif
+                    <button class="btn-add-to-cart" type="submit">Thêm vào giỏ hàng</button>
+                </form>
+            </div>
                     </div>
                 </div>
                 @endforeach
