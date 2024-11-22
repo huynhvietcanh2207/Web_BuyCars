a8fbddd4d54d28977805d93bf661cb00e9c8b2f2
DucSinhVien <22211tt0896@mail.tdc.edu.vn>
2024-11-23 00:22:10 +0700

    Update

--- DoAn_BuyCars/resources/views/detail.blade.php
+++ DoAn_BuyCars/resources/views/detail.blade.php
@@ -6,102 +6,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DETAIL - {{ $product->name }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+        </script>
+
     <link rel="stylesheet" href="{{ url('css/detail.css') }}">
 </head>
 
 <body>
     @section('main')
-    <div class="container mt-5">
+    <div class="banner">
+        <div class="cart-header text-center">
+            <img src="{{ asset('images/banner1.jpg') }}" alt="Banner">
+            <h1>GHI TIẾT SẢN PHẨM</h1>
+            <p><span id="hightlight">Trang chủ ></span> Chi tiết sản phẩm: <strong>{{ $product->name }}</strong></p>
+        </div>
+    </div>
+    <div class="container detailform">
         <div class="row">
-            <!-- Product Image -->
-            <div class="col-md-6">
-                <!-- Main product image -->
+            <div class="col-md-6 imgdetail">
                 <div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="{{ asset('images/' . $product->image_url . '.jpg') }}" class="d-block"
-                                alt="{{ $product->name }}">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{ asset('images/lambo.jpg') }}" class="d-block w-100"
-                                alt="Nhớt Castrol Magnatec 10W40 - 2">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{ asset('images/banner1.jpg') }}" class="d-block w-100"
-                                alt="Nhớt Castrol Magnatec 10W40 - 3">
+                            <img src="{{ asset($product->image_url) }}" class="d-block" alt="{{ $product->name }}">
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#productCarousel"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-
-                <!-- Thumbnails below the carousel -->
-                <div class="mt-3 d-flex justify-content-center">
-                    <img src="{{ asset('images/56.jpg') }}" class="img-thumbnail me-2" style="width: 100px;"
-                        data-bs-target="#productCarousel" data-bs-slide-to="0" alt="Nhớt Castrol Magnatec 10W40">
-                    <img src="{{ asset('images/lambo.jpg') }}" class="img-thumbnail me-2" style="width: 100px;"
-                        data-bs-target="#productCarousel" data-bs-slide-to="1" alt="Nhớt Castrol Magnatec 10W40 - 2">
-                    <img src="{{ asset('images/banner1.jpg') }}" class="img-thumbnail" style="width: 100px;"
-                        data-bs-target="#productCarousel" data-bs-slide-to="2" alt="Nhớt Castrol Magnatec 10W40 - 3">
                 </div>
             </div>
+            <div class="col-md-1 line"></div>
             <!-- Product Information -->
-            <div class="col-md-6">
-                <h3>{{ $product->name }}</h3>
-                <p>Thương hiệu: Đang cập nhật | Tình trạng: <span class="text-success">Còn hàng</span></p>
-                <h4 class="text-danger">{{ number_format($product->price, 0, ',', '.') }} VND</h4>
-                <div class="input-group mb-3">
-                    <button class="btn btn-outline-secondary" type="button">-</button>
-                    <input type="text" class="form-control text-center" value="1" style="max-width: 60px;">
-                    <button class="btn btn-outline-secondary" type="button">+</button>
-                </div>
-                {{-- <button class="btn btn-danger mb-3">Đặt hàng</button> --}}
-                <form action="{{ route('cart.add', $product->ProductId) }}" method="POST">
-                    @csrf
-                    <input type="hidden" name="product_id" value="{{ $product->ProductId }}">
-
-                    @if (auth()->check())
-                        <input type="hidden" name="user_id" value="{{ auth()->id() }}">
-                    @endif
-                    <button class="btn-add-to-cart" data-id="{{ $product->ProductId }}"
-                        data-price="{{ $product->price }}">Thêm vào giỏ hàng</button>
-                </form>
-                <p>{{ $product->description }}</p>
-                <div class="d-flex">
-                    <span class="me-2">Chia sẻ: </span>
-                    <a href="#" class="me-2"><i class="fa fa-facebook"></i></a>
-                    <a href="#" class="me-2"><i class="bi bi-twitter"></i></a>
-                    <a href="#" class="me-2"><i class="bi bi-google"></i></a>
+            <div class="col-md-5">
+                <div class="detailinfo">
+                    <h3>{{ $product->name }}</h3>
+                    <p>Thương hiệu: Đang cập nhật...</p>
+                    <h4 class="text-danger pricedetail">{{ number_format($product->price, 0, ',', '.') }} VNĐ</h4>
+                    <div class="input-group mb-3">
+                        <button class="btn btn-outline-secondary" type="button">-</button>
+                        <input type="text" class="form-control text-center" value="1" style="max-width: 60px;" min="1">
+                        <button class="btn btn-outline-secondary" type="button">+</button>
+                    </div>
+                    <form action="{{ route('cart.add', $product->ProductId) }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="product_id" value="{{ $product->ProductId }}">
+                        @if (auth()->check())
+                            <input type="hidden" name="user_id" value="{{ auth()->id() }}">
+                        @endif
+                        <button class="btn-add-to-cart" type="submit"><strong>THÊM GIỎ HÀNG</strong></button>
+                    </form>
                 </div>
             </div>
         </div>
         <div class="row mt-5">
             <div class="col-md-12">
-                <ul class="nav nav-tabs">
+                <ul class="nav nav-tabs product-info-tabs">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Thông tin sản phẩm</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Chính sách</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Đánh giá sản phẩm</a>
+                        <a class="nav-link active"><strong>THÔNG TIN SẢN PHẨM</strong></a>
                     </li>
                 </ul>
-                <div class="p-3">
-                    <h5>{{ $product->name }}</h5>
-                    <ul>
-                        <li>{{ $product->description }}</li>
-                    </ul>
+                <div class="p-3 detailinfoproduct">
+                    <h5><span><strong id="namecar">TÊN XE: </strong></span>{{ $product->name }}</h5>
+                    <div class="product-description">
+                        <p><i class="fa fa-caret-right"></i>
+                            <span class="short-description"> {!! \Str::limit(strip_tags($product->description), 250)
+                                !!}</span>
+                            <span class="full-description" style="display:none;">{!! $product->description !!}</span>
+                            <a href="javascript:void(0);" class="toggle-description">Xem thêm...</a>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
